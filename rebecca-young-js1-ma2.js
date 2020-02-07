4eb40fb8ca4346f861f4c8d868f97316226a47c4
Bekki Young <moonbladez@hotmail.com>
2020-02-07 23:35:26 +0100

    question 1

--- rebecca-young-js1-ma2.js
+++ rebecca-young-js1-ma2.js
@@ -1,2 +1,15 @@
 //question 1
+const myFunctionExpression = function () {
+    console.log("Rebecca Young")
+}
 
+myFunctionExpression()
+
+//question 2
+const button = document.getElementsByClassName("btn")
+
+function btnClick() {
+    console.log("click me")
+}
+
+button.addEventListener("click", btnClick)
